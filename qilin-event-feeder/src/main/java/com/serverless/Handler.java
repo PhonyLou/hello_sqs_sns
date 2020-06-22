7604ab7f4a39c146bfeccf25929ac6e548ab6d9f
Phony Lou <louqilin555@gmail.com>
2020-06-22 22:26:08 +0800

    Can display logs

--- qilin-event-feeder/src/main/java/com/serverless/Handler.java
+++ qilin-event-feeder/src/main/java/com/serverless/Handler.java
@@ -1,6 +1,7 @@
 package com.serverless;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import org.apache.log4j.Logger;
@@ -11,25 +12,9 @@ public class Handler implements RequestHandler<SQSEvent, ApiGatewayResponse> {
 
 	@Override
 	public ApiGatewayResponse handleRequest(SQSEvent event, Context context) {
-		LOG.info("fdsafdsafdsafdsa");
-		LOG.info("received: " + event);
-//		final AmazonCloudWatch cw =
-//				AmazonCloudWatchClientBuilder.defaultClient();
-//		Dimension dimension = new Dimension()
-//				.withName("FeederInstance")
-//				.withValue("qilin-metrics-feeder");
-//
-//		MetricDatum datum = new MetricDatum()
-//				.withMetricName("Lazy_Metrics")
-//				.withUnit(StandardUnit.None)
-//				.withValue(new Random().nextDouble() * 10)
-//				.withDimensions(dimension);
-//
-//		PutMetricDataRequest request = new PutMetricDataRequest()
-//				.withNamespace("Qilin/Metrics")
-//				.withMetricData(datum);
-//
-//		PutMetricDataResult response = cw.putMetricData(request);
+		LambdaLogger logger = context.getLogger();
+		logger.log("fdsafdsafdsafdsa");
+		logger.log("received: " + event);
 
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
